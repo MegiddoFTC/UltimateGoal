c2957c370a7f5ba05b9284cbd8f872d846bd1d37
MegiddoFTC <59795243+MegiddoFTC@users.noreply.github.com>
2021-03-21 18:18:16 +0200

    basic AutoDefault program

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Default.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Default.java
@@ -7,29 +7,29 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 
 public abstract class Default extends OpMode {
 
-    protected DcMotor rearLeft;
-    protected DcMotor frontLeft;
-    protected DcMotor rearRight;
+    protected DcMotor rearLeft  ;
+    protected DcMotor frontLeft ;
+    protected DcMotor rearRight ;
     protected DcMotor frontRight;
 
     protected DcMotor pumpMotor;
 
     @Override
     public void init() {
-        rearLeft = hardwareMap.dcMotor.get("RearLeft");
-        frontLeft = hardwareMap.dcMotor.get("FrontLeft");
-        rearRight = hardwareMap.dcMotor.get("RearRight");
+        rearLeft = hardwareMap.dcMotor.get("RearLeft")    ;
+        frontLeft = hardwareMap.dcMotor.get("FrontLeft")  ;
+        rearRight = hardwareMap.dcMotor.get("RearRight")  ;
         frontRight = hardwareMap.dcMotor.get("FrontRight");
-        pumpMotor = hardwareMap.dcMotor.get("PumpMotor");
+        pumpMotor = hardwareMap.dcMotor.get("PumpMotor")  ;
 
-        rearLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE);
-        rearRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        rearLeft.setDirection(DcMotorSimple.Direction.REVERSE)  ;
+        frontLeft.setDirection(DcMotorSimple.Direction.REVERSE) ;
+        rearRight.setDirection(DcMotorSimple.Direction.FORWARD) ;
         frontRight.setDirection(DcMotorSimple.Direction.FORWARD);
-        pumpMotor.setDirection(DcMotorSimple.Direction.FORWARD);
+        pumpMotor.setDirection(DcMotorSimple.Direction.FORWARD) ;
 
         setDriveRunMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-        setDriveRunMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        setDriveRunMode(DcMotor.RunMode.RUN_USING_ENCODER)     ;
 
         setDriveZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
     }
@@ -47,23 +47,23 @@ public abstract class Default extends OpMode {
     }
 
     protected void powerMotors(double RLeft, double FLeft, double RRight, double FRight) { // set power
-        rearLeft.setPower(-RLeft);
-        frontLeft.setPower(-FLeft);
-        rearRight.setPower(-RRight);
+        rearLeft.setPower(-RLeft)   ;
+        frontLeft.setPower(-FLeft)  ;
+        rearRight.setPower(-RRight) ;
         frontRight.setPower(-FRight);
     }
 
     protected void setDriveZeroPowerBehavior(DcMotor.ZeroPowerBehavior zeroPowerBehavior) {
-        rearLeft.setZeroPowerBehavior(zeroPowerBehavior);
-        frontLeft.setZeroPowerBehavior(zeroPowerBehavior);
-        rearRight.setZeroPowerBehavior(zeroPowerBehavior);
+        rearLeft.setZeroPowerBehavior(zeroPowerBehavior)  ;
+        frontLeft.setZeroPowerBehavior(zeroPowerBehavior) ;
+        rearRight.setZeroPowerBehavior(zeroPowerBehavior) ;
         frontRight.setZeroPowerBehavior(zeroPowerBehavior);
     }
 
     protected void setDriveRunMode(DcMotor.RunMode runMode) {
-        rearLeft.setMode(runMode);
-        frontLeft.setMode(runMode);
-        rearRight.setMode(runMode);
+        rearLeft.setMode(runMode)  ;
+        frontLeft.setMode(runMode) ;
+        rearRight.setMode(runMode) ;
         frontRight.setMode(runMode);
     }
 
